ffc9ed6353bff9de92531c921a936dc1d8746545
GoofysM8 <mattsanderson98@gamil.com>
2023-02-13 14:09:42 +0000

    Fixed typo and added input values

--- data_visualization/mpl_squares.py
+++ data_visualization/mpl_squares.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
+input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 fig, ax = plt.subplots()
-ax.plot(squares, linewidth=3)
+ax.plot(input_values, squares, linewidth=3)
 
 #Set chart title and label axes
 ax.set_title("Square Numbers", fontsize=24)
@@ -10,6 +11,6 @@ ax.set_xlabel("Vaule", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
 
 #Set size of tick labels
-ax.tick_params(axis='both', labelsixe=14)
+ax.tick_params(axis='both', labelsize=14)
 
 plt.show()
